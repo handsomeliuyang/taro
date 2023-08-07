6fe84d9d8a6bcbefe685579b4402e38c19c18051
tangcq-code <1192601676@qq.com>
2023-08-07 10:45:44 +0800

    fix: 修改陀螺仪的api实现和playground示例

--- examples/mini-program-example/src/pages/api/device/gyroscope/index.tsx
+++ examples/mini-program-example/src/pages/api/device/gyroscope/index.tsx
@@ -2,45 +2,51 @@ import React from 'react'
 import Taro from '@tarojs/taro'
 import { View, Text } from '@tarojs/components'
 import './index.scss'
+import { TestConsole } from '@/util/util'
 
 /**
  * 设备-陀螺仪
  * @returns
  */
-
 export default class Index extends React.Component {
   state = {
     list: [
       {
         id: 'stopGyroscope',
         func: () => {
+          TestConsole.consoleTest('stopGyroscope')
           Taro.stopGyroscope({
             success: (res) => {
-              console.log('success-----', res)
+              TestConsole.consoleSuccess(res)
             },
             fail: (res) => {
-              console.log('fail-----', res)
+              TestConsole.consoleFail(res)
             },
             complete: (res) => {
-              console.log('complete-----', res)
+              TestConsole.consoleComplete(res)
             },
+          }).then((ret) => {
+            TestConsole.consoleReturn(ret)
           })
         },
       },
       {
         id: 'startGyroscope',
         func: () => {
+          TestConsole.consoleTest('startGyroscope')
           Taro.startGyroscope({
+            interval: 'normal',
             success: (res) => {
-              console.log('success-----', res)
+              TestConsole.consoleSuccess(res)
             },
             fail: (res) => {
-              console.log('fail-----', res)
+              TestConsole.consoleFail(res)
             },
             complete: (res) => {
-              console.log('complete-----', res)
-            },
-            interval: 'normal',
+              TestConsole.consoleComplete(res)
+            }
+          }).then((ret) => {
+            TestConsole.consoleReturn(ret)
           })
         },
       },
@@ -48,26 +54,26 @@ export default class Index extends React.Component {
         id: 'onGyroscopeChange',
         func: () => {
           Taro.onGyroscopeChange((res) => {
-            console.log('on gyroscope change: ', res)
+            TestConsole.consoleNormal('on gyroscope change: ', res)
           })
         },
       },
       {
-        id: 'offGyroscopeChange',
+        id: 'offGyroscopeChange_暂不支持',
         func: () => {
           Taro.offGyroscopeChange((res) => {
-            console.log('off gyroscope change: ', res)
+            TestConsole.consoleNormal('off gyroscope change: ', res)
           })
         },
       },
     ],
   }
-  render() {
+  render () {
     return (
       <View className='api-page'>
         {this.state.list.map((item) => {
           return (
-            <View key={item.id} className='api-page-btn' onClick={item.func == null ? () => {} : item.func}>
+            <View key={item.id} className='api-page-btn' onClick={item.func == null ? () => { } : item.func}>
               {item.id}
               {item.func == null && <Text className='navigator-state tag'>未创建Demo</Text>}
             </View>
