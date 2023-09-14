dce257e7a264b1d06aef695a2672ba97b45f4abb
liujie <491818890@qq.com>
2023-09-14 15:23:05 +0800

    fix: wxml的px/rpx转为rem

--- packages/taroize/src/wxml.ts
+++ packages/taroize/src/wxml.ts
@@ -171,6 +171,30 @@ export const createWxmlVistor = (
       return
     }
 
+    // 尺寸单位转换 都转为rem : 1rpx 转为 1/40rem,,1px 转为 1/20rem
+    if (t.isStringLiteral(path.node.value)) {
+      let tempValue = path.node.value.value
+      if (tempValue.indexOf('px') !== -1) {
+        // 把 xxx="...[数字]rpx/px" 的尺寸单位都转为 rem, 转换方法类似postcss-taro-unit-transform
+        tempValue = tempValue
+          .replace(/([0-9.]+)px/gi, function (match) {
+            return parseInt(match, 10) / 20 + 'rem'
+          })
+          .replace(/([0-9.]+)rpx/gi, function (match) {
+            return parseInt(match, 10) / 40 + 'rem'
+          })
+        // 把 xx="...{{参数}}rpx/px"的尺寸单位都转为rem,比如"{{参数}}rpx" -> "{{参数/40}}rem"
+        tempValue = tempValue
+          .replace(/\{\{([^{}]*)\}\}(px)/gi, function (match, size, unit) {
+            return match.replace(size, size + '/20').replace(unit, 'rem')
+          })
+          .replace(/\{\{([^{}]*)\}\}(rpx)/gi, function (match, size, unit) {
+            return match.replace(size, size + '/40').replace(unit, 'rem')
+          })
+        path.node.value.value = tempValue
+      }
+    }
+
     // 把 style 中 {{}} 转为 ${} 格式
     if (name.name === 'style' && t.isStringLiteral(path.node.value)) {
       const styleAttrsMap: any[] = []
