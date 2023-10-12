80ec0bb5ea4276c480b4b359de79b12bcb54fad7
fahasikei <1299484460@qq.com>
2023-10-12 16:17:52 +0800

    fix: 补充style属性测试用例，以及修复event事件部分转换问题

--- packages/taroize/__tests__/wxml.test.ts
+++ packages/taroize/__tests__/wxml.test.ts
@@ -1,4 +1,6 @@
-import { parseContent,parseWXML } from '../src/wxml'
+import * as t from '@babel/types'
+
+import { convertStyleUnit,parseContent, parseStyle, parseWXML } from '../src/wxml'
 
 interface Option {
   path: string
@@ -65,5 +67,285 @@ describe('parseContent', () => {
     const { type, content } = parseContent(contentInput)
     expect(type).toBe('expression')
     expect(content).toBe(`(list)`)
+  })
+})
+
+describe('style属性的解析', () => {
+  // 第一种以style="xxx:xxx"的用法
+  test('style = xxx:xxx', () => {
+    let contentInput = 'color: red;background-color: aqua;'
+    contentInput = convertStyleUnit(contentInput)
+    const styleParseReslut = parseStyle('style', contentInput)
+    if (t.isJSXAttribute(styleParseReslut)) {
+      expect(styleParseReslut.type).toEqual('JSXAttribute')
+    } else {
+      const { type, content } = styleParseReslut
+      expect(type).toBe('expression')
+      expect(content).toBe('(color: red;background-color: aqua;)')
+    }
+  })
+
+  test('style = xxx:{{ xxx }}', () => {
+    let contentInput = 'color: red;background-color: aqua;font-size: {{ fontSize }};'
+    contentInput = convertStyleUnit(contentInput)
+    const styleParseReslut = parseStyle('style', contentInput)
+    if (t.isJSXAttribute(styleParseReslut)) {
+      expect(styleParseReslut.type).toEqual('JSXAttribute')
+    } else {
+      const { type, content } = styleParseReslut
+      expect(type).toBe('expression')
+      expect(content).toBe('(color: red;background-color: aqua;font-size: {{ fontSize }};)')
+    }
+  })
+
+  test('style = xxx:{{ xxx }}px', () => {
+    let contentInput = 'height: {{ height }}px;width: 100px;background-color: red;'
+    contentInput = convertStyleUnit(contentInput)
+    const styleParseReslut = parseStyle('style', contentInput)
+    if (t.isJSXAttribute(styleParseReslut)) {
+      expect(styleParseReslut.type).toEqual('JSXAttribute')
+    } else {
+      const { type, content } = styleParseReslut
+      expect(type).toBe('expression')
+      expect(content).toBe('(height: {{ height }}px;width: 5rem;background-color: red;)')
+    }
+  })
+
+  test('style = xxx:{{ xxx }}, 含有三元运算符', () => {
+    let contentInput = 'height: {{ list.length > 10 ? 200 : 100 }}px;width: 100px;background-color: green;'
+    contentInput = convertStyleUnit(contentInput)
+    const styleParseReslut = parseStyle('style', contentInput)
+    if (t.isJSXAttribute(styleParseReslut)) {
+      expect(styleParseReslut.type).toEqual('JSXAttribute')
+    } else {
+      const { type, content } = styleParseReslut
+      expect(type).toBe('expression')
+      expect(content).toBe('(height: {{ list.length > 10 ? 200 : 100 }}px;width: 100px;background-color: green);')
+    }
+  })
+
+  test('style = xxx:{{ xxx + 10 }}px', () => {
+    let contentInput = 'height: {{ height + 10 }}px;width: 100px;background-color: red;'
+    contentInput = convertStyleUnit(contentInput)
+    const styleParseReslut = parseStyle('style', contentInput)
+    if (t.isJSXAttribute(styleParseReslut)) {
+      expect(styleParseReslut.type).toEqual('JSXAttribute')
+    } else {
+      const { type, content } = styleParseReslut
+      expect(type).toBe('expression')
+      expect(content).toBe('({{ height + 10 }}px;width: 5rem;background-color: red;)')
+    }
+  })
+
+  test('style = xxx:xxx混用情况1', () => {
+    let contentInput = 'height: {{ height }}px;width: {{ width }}px;background-color: {{ color }};'
+    contentInput = convertStyleUnit(contentInput)
+    const styleParseReslut = parseStyle('style', contentInput)
+    if (t.isJSXAttribute(styleParseReslut)) {
+      expect(styleParseReslut.type).toEqual('JSXAttribute')
+    } else {
+      const { type, content } = styleParseReslut
+      expect(type).toBe('expression')
+      expect(content).toBe('(height: {{ height }}px;width: {{ width }}px;background-color: {{ color }})')
+    }
+  })
+
+  test('style = xxx:xxx混用情况2', () => {
+    let contentInput = 'height: {{ height }}px;width: {{ width }}px;background-color: {{ false ? color : "blue" }};'
+    contentInput = convertStyleUnit(contentInput)
+    const styleParseReslut = parseStyle('style', contentInput)
+    if (t.isJSXAttribute(styleParseReslut)) {
+      expect(styleParseReslut.type).toEqual('JSXAttribute')
+    } else {
+      const { type, content } = styleParseReslut
+      expect(type).toBe('expression')
+      expect(content).toBe('(height: {{ height }}px;width: {{ width }}px;background-color: {{ false ? color : "blue" }};)')
+    }
+  })
+
+  // 第二种以双括号用法
+  test('style = {{ xxx }}变量形式', () => {
+    let contentInput = '{{ myStyle }}'
+    contentInput = convertStyleUnit(contentInput)
+    const styleParseReslut = parseStyle('style', contentInput)
+    if (t.isJSXAttribute(styleParseReslut)) {
+      expect(styleParseReslut).toEqual(t.jSXAttribute)
+    } else {
+      const { type, content } = styleParseReslut
+      expect(type).toBe('expression')
+      expect(content).toBe('(myStyle)')
+    }
+  })
+
+  test('style = {{ xxx }}字符串形式', () => {
+    let contentInput = "{{ 'height: 100px;width: 100px;background-color: red;' }}"
+    contentInput = convertStyleUnit(contentInput)
+    const styleParseReslut = parseStyle('style', contentInput)
+    if (t.isJSXAttribute(styleParseReslut)) {
+      expect(styleParseReslut).toEqual(t.jSXAttribute)
+    } else {
+      const { type, content } = styleParseReslut
+      expect(type).toBe('expression')
+      expect(content).toBe("('height: 5rem;width: 5rem;background-color: red;')")
+    }
+  })
+
+  test('style = {{ xxx }}含三元运算符', () => {
+    let contentInput = "{{ index > 1 ? 'height: 100px;width: 100px;background-color: green;' : '' }}"
+    contentInput = convertStyleUnit(contentInput)
+    const styleParseReslut = parseStyle('style', contentInput)
+    if (t.isJSXAttribute(styleParseReslut)) {
+      expect(styleParseReslut).toEqual(t.jSXAttribute)
+    } else {
+      const { type, content } = styleParseReslut
+      expect(type).toBe('expression')
+      expect(content).toBe("(index > 1 ? 'height: 5rem;width: 5rem;background-color: green;' : '')")
+    }
+  })
+
+  test('style = {{ xxx }}含三元运算符和变量', () => {
+    let contentInput = '{{ index ? "background-color: " + backgroundColor + ";" : "" }}'
+    contentInput = convertStyleUnit(contentInput)
+    const styleParseReslut = parseStyle('style', contentInput)
+    if (t.isJSXAttribute(styleParseReslut)) {
+      expect(styleParseReslut).toEqual(t.jSXAttribute)
+    } else {
+      const { type, content } = styleParseReslut
+      expect(type).toBe('expression')
+      expect(content).toBe('(index ? "background-color: " + backgroundColor + ";" : "")')
+    }
+  })
+
+  // 第三种xxx:xxx和{{ xxx }}混用情况
+  test('xxx:xxx和{{ xxx }}混用情况1', () => {
+    let contentInput = 'background-color: aqua;{{ global_css }}'
+    contentInput = convertStyleUnit(contentInput)
+    const styleParseReslut = parseStyle('style', contentInput)
+    if (t.isJSXAttribute(styleParseReslut)) {
+      expect(styleParseReslut).toEqual(t.jSXAttribute)
+    } else {
+      const { type, content } = styleParseReslut
+      expect(type).toBe('expression')
+      expect(content).toBe('"background-color: aqua;"+(global_css)')
+    }
+  })
+
+  test('xxx:xxx和{{ xxx }}混用情况2', () => {
+    let contentInput = 'background-color: aqua;{{ true ? global_css : "" }}'
+    contentInput = convertStyleUnit(contentInput)
+    const styleParseReslut = parseStyle('style', contentInput)
+    if (t.isJSXAttribute(styleParseReslut)) {
+      expect(styleParseReslut).toEqual(t.jSXAttribute)
+    } else {
+      const { type, content } = styleParseReslut
+      expect(type).toBe('expression')
+      expect(content).toBe('"background-color: aqua;"+(true ? global_css : "")')
+    }
+  })
+
+  test('xxx:xxx和{{ xxx }}混用情况3', () => {
+    let contentInput = 'background-color: {{ color }};{{ global_css }}'
+    contentInput = convertStyleUnit(contentInput)
+    const styleParseReslut = parseStyle('style', contentInput)
+    if (t.isJSXAttribute(styleParseReslut)) {
+      expect(styleParseReslut).toEqual(t.jSXAttribute)
+    } else {
+      const { type, content } = styleParseReslut
+      expect(type).toBe('expression')
+      expect(content).toBe('"background-color: "+(color)+";"+(global_css)')
+    }
+  })
+
+  test('xxx:xxx和{{ xxx }}混用情况4', () => {
+    let contentInput = 'background-color: {{ true ? color : "" }};{{ global_css }}'
+    contentInput = convertStyleUnit(contentInput)
+    const styleParseReslut = parseStyle('style', contentInput)
+    if (t.isJSXAttribute(styleParseReslut)) {
+      expect(styleParseReslut).toEqual(t.jSXAttribute)
+    } else {
+      const { type, content } = styleParseReslut
+      expect(type).toBe('expression')
+      expect(content).toBe('"background-color: "+(true ? color : "")+";"+(global_css)')
+    }
+  })
+
+  test('xxx:xxx和{{ xxx }}混用情况5', () => {
+    let contentInput = '{{ global_css }}{{ myStyle }}'
+    contentInput = convertStyleUnit(contentInput)
+    const styleParseReslut = parseStyle('style', contentInput)
+    if (t.isJSXAttribute(styleParseReslut)) {
+      expect(styleParseReslut).toEqual(t.jSXAttribute)
+    } else {
+      const { type, content } = styleParseReslut
+      expect(type).toBe('expression')
+      expect(content).toBe('(global_css)+(myStyle)')
+    }
+  })
+
+  // 第四种情况,与第一种情况有差别
+  test('style = xxx:xxx, 但是key是变量', () => {
+    let contentInput = '{{ styleName }}: red;height: 100px;width: 100px;'
+    contentInput = convertStyleUnit(contentInput)
+    const styleParseReslut = parseStyle('style', contentInput)
+    if (t.isJSXAttribute(styleParseReslut)) {
+      expect(styleParseReslut).toEqual(t.jSXAttribute)
+    } else {
+      const { type, content } = styleParseReslut
+      expect(type).toBe('expression')
+      expect(content).toBe('(styleName)+": red;height: 5rem;width: 5rem;"')
+    }
+  })
+
+  // style为空的特殊写法
+  test('style为空的特殊写法1', () => {
+    let contentInput = '{styleX}'
+    contentInput = convertStyleUnit(contentInput)
+    const styleParseReslut = parseStyle('style', contentInput)
+    if (t.isJSXAttribute(styleParseReslut)) {
+      expect(styleParseReslut).toEqual(t.jSXAttribute)
+    } else {
+      const { type, content } = styleParseReslut
+      expect(type).toBe('raw')
+      expect(content).toBe('{styleX}')
+    }
+  })
+
+  test('style为空的特殊写法2', () => {
+    let contentInput = '""'
+    contentInput = convertStyleUnit(contentInput)
+    const styleParseReslut = parseStyle('style', contentInput)
+    if (t.isJSXAttribute(styleParseReslut)) {
+      expect(styleParseReslut).toEqual(t.jSXAttribute)
+    } else {
+      const { type, content } = styleParseReslut
+      expect(type).toBe('raw')
+      expect(content).toBe('""')
+    }
+  })
+
+  test('style为空的特殊写法3', () => {
+    let contentInput = '{{}}'
+    contentInput = convertStyleUnit(contentInput)
+    const styleParseReslut = parseStyle('style', contentInput)
+    if (t.isJSXAttribute(styleParseReslut)) {
+      expect(styleParseReslut).toEqual(t.jSXAttribute)
+    } else {
+      const { type, content } = styleParseReslut
+      expect(type).toBe('raw')
+      expect(content).toBe('{{}}')
+    }
+  })
+
+  test('style为空的特殊写法4', () => {
+    let contentInput = '{{ "" }}'
+    contentInput = convertStyleUnit(contentInput)
+    const styleParseReslut = parseStyle('style', contentInput)
+    if (t.isJSXAttribute(styleParseReslut)) {
+      expect(styleParseReslut).toEqual(t.jSXAttribute)
+    } else {
+      const { type, content } = styleParseReslut
+      expect(type).toBe('expression')
+      expect(content).toBe('("")')
+    }
   })
 })
