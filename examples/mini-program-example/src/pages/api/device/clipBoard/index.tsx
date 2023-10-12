345b0a46f4dca863b315889b1b71a31331733283
tangcq-code <1192601676@qq.com>
2023-10-12 20:46:14 +0800

    fix: 修复执行setClipBoardData接口，手机没有弹出提示内容

--- examples/mini-program-example/src/pages/api/device/clipBoard/index.tsx
+++ examples/mini-program-example/src/pages/api/device/clipBoard/index.tsx
@@ -24,11 +24,6 @@ export default class Index extends React.Component {
             ...data,
             success: (res) => {
               TestConsole.consoleSuccess.call(this, res, apiIndex)
-              Taro.showToast({
-                title: '内容已复制',
-                icon: 'none',
-                duration: 2000
-              })
             },
             fail: (res) => {
               TestConsole.consoleFail.call(this, res, apiIndex)
